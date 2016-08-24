1140640193c030feb84447bf02bd33c94f8f2118
aberry5621 <aberry5621@mycom.marin.edu>
2016-08-24 10:48:18 -0700

    checkpoint_excercise_1-32.cpp
    from  the book

---
+++ checkpoint_excercise_1-32.cpp
@@ -0,0 +1,19 @@
+//
+//  checkpoint_excercise_1-32.cpp
+//  skratch
+//
+//  Created by ax on 8/24/16.
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "C++" << "Java" << endl;
+    cout << "C++" << endl << "Java" << endl;
+    cout << "C++, " << "Java, " << "and C#" << endl;
+    
+    return 0;
+}
