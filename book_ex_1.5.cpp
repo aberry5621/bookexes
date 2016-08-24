a4205ef00b7f70e5ca3303f03f87734edc43693e
aberry5621 <aberry5621@mycom.marin.edu>
2016-08-24 11:07:56 -0700

    Book Exercise 1.5 - Tested by manual check

---
+++ book_ex_1.5.cpp
@@ -0,0 +1,15 @@
+//
+//  Book excercise 1.5
+//  skratch
+//
+//  Created by ax on 8/24/16.
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "9.5 x 4.5 - 2.5 x 3 / 45.5 - 3.5 = " << ((9.5 * 4.5) - (2.5 * 3))/(45.5 - 3.5) << endl;
+}
