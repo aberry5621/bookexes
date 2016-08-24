4ff4bd2e39c9a104f9067f783f60129d055f712c
aberry5621 <aberry5621@mycom.marin.edu>
2016-08-24 11:10:28 -0700

    Book excercise 1.6

---
+++ book_ex_1.6.cpp
@@ -0,0 +1,15 @@
+//
+//  Book excercise 1.6
+//  skratch
+//
+//  Created by ax on 8/24/16.
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " << 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 << endl;
+}
