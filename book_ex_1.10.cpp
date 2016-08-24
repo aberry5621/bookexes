7babf53998094c52de6f331915e275179ed0b082
aberry5621 <aberry5621@mycom.marin.edu>
2016-08-24 11:46:01 -0700

    book exercise 1.10

---
+++ book_ex_1.10.cpp
@@ -0,0 +1,22 @@
+//
+//  Book exercise 1.10
+//  skratch
+//
+//  Created by ax on 8/24/16.
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Average Speed Conversion KPH to MPH; Runner Runs 14K in 45:30" << endl;
+    
+    cout << "Avg Kph: " << (45.5 / 14) << endl;
+    
+    cout << "Avg Mph: " << (45.5 / 14) / 1.6 << endl;
+    
+    return 0;
+
+}
